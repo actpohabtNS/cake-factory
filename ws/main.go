5446a54e95e8fc8c98fa81872cdff0733821a230
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-10-02 13:52:45 +0300

    [Add] web Socket usage

---
+++ ws/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"cake-factory/jwt"
+	"flag"
+	"log"
+	"net/http"
+	"strings"
+)
+
+var addr = flag.String("addr", ":8081", "http service address")
+
+func main() {
+	flag.Parse()
+	hub := NewHub()
+	go hub.run()
+	go hub.receive()
+
+	jwtService, err := jwt.NewJWTService("pubkey.rsa", "privkey.rsa")
+	if err != nil {
+		panic(err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		token := strings.TrimPrefix(authHeader, "Bearer ")
+		if _, err := jwtService.ParseJWT(token); err != nil {
+			w.WriteHeader(401)
+			w.Write([]byte("unauthorized"))
+			return
+		}
+
+		serveWS(hub, w, r)
+	})
+
+	err = http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
